10ba02896a89f2e2ae516227de42ecde292fdbca
Sam YOU <samtwo68772@gmail.com>
2025-09-19 07:07:51 +0000

    you make the containers horizinbtal  size small make wide

--- src/app/admin/settings/page.tsx
+++ src/app/admin/settings/page.tsx
@@ -23,7 +23,7 @@ export default function AdminSettingsPage() {
 
     return (
         <div className="flex flex-col h-full gap-6 px-6 py-6">
-            <div className="grid gap-6 max-w-xl mx-auto w-full">
+            <div className="grid gap-6 mx-auto w-full max-w-4xl">
                 <Card>
                     <CardHeader>
                         <CardTitle>Profile Information</CardTitle>
